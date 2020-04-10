d3bb7a84523f83a9c0cf7caa2bb6802609ad066a
Menno van Scheers <menno.van.scheers@gmail.com>
2020-04-10 16:29:57 +0200

    Initial commit

--- Saitawa/Program.cs
+++ Saitawa/Program.cs
@@ -14,7 +14,7 @@ namespace Saitawa
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new Saitawa())
                 game.Run();
         }
     }
--- Saitawa/Saitawa.cs
+++ Saitawa/Saitawa.cs
@@ -7,12 +7,12 @@ namespace Saitawa
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
-    public class Game1 : Game
+    public class Saitawa : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        public Game1()
+        public Saitawa()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
